8e3be1ebda61699106f0b1948f271e5ce3590711
Štěpán Bořek <stepan.borek@gmail.com>
2020-09-29 11:36:47 +0200

    update 1.1.0

--- CalamityChargerRecipe/CalamityChargerRecipe.cs
+++ CalamityChargerRecipe/CalamityChargerRecipe.cs
@@ -9,14 +9,34 @@ namespace CalamityChargerRecipe
 		public override void AddRecipes()
 		{
 			Mod calamityMod = ModLoader.GetMod("CalamityMod");
-			ModRecipe recipe = new ModRecipe(this);
+			
+			ModRecipe recipeCharger = new ModRecipe(this);
 			if (calamityMod != null) {
-				recipe.AddIngredient(calamityMod.ItemType("DraedonsFuelFactoryItem"));
-				recipe.AddIngredient(calamityMod.ItemType("DraedonsChargingStation"));
-				recipe.AddIngredient(calamityMod.ItemType("DubiousPlating"), 10);
-				recipe.AddIngredient(calamityMod.ItemType("MysteriousCircuitry"), 10);
-				recipe.SetResult(calamityMod.ItemType("ChargerTestItem"), 1);
-				recipe.AddRecipe();
+				recipeCharger.AddTile(TileID.Anvils);
+				recipeCharger.AddIngredient(calamityMod.ItemType("DraedonsFuelFactoryItem"));
+				recipeCharger.AddIngredient(calamityMod.ItemType("DraedonsChargingStation"));
+				recipeCharger.AddIngredient(calamityMod.ItemType("DubiousPlating"), 10);
+				recipeCharger.AddIngredient(calamityMod.ItemType("MysteriousCircuitry"), 10);
+				recipeCharger.SetResult(calamityMod.ItemType("ChargerTestItem"), 1);
+				recipeCharger.AddRecipe();
+			}
+			
+			ModRecipe recipeDubiousPlating = new ModRecipe(this);
+			if (calamityMod != null) {
+				recipeDubiousPlating.AddTile(TileID.Anvils);
+				recipeDubiousPlating.AddIngredient(ItemID.Glass);
+				recipeDubiousPlating.AddRecipeGroup("IronBar", 1);
+				recipeDubiousPlating.SetResult(calamityMod.ItemType("DubiousPlating"), 1);
+				recipeDubiousPlating.AddRecipe();
+			}
+			
+			ModRecipe recipeMysteriousCircuitry = new ModRecipe(this);
+			if (calamityMod != null) {
+				recipeMysteriousCircuitry.AddTile(TileID.Anvils);
+				recipeMysteriousCircuitry.AddIngredient(ItemID.Glass);
+				recipeMysteriousCircuitry.AddRecipeGroup("IronBar", 1);
+				recipeMysteriousCircuitry.SetResult(calamityMod.ItemType("MysteriousCircuitry"), 1);
+				recipeMysteriousCircuitry.AddRecipe();
 			}
 		}
 	}
